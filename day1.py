461cb61ae69aa2d3cd0b707a98d050b429cf8923
Aiuraa <aiuramotoyasu@gmail.com>
2022-12-02 03:15:33 +0700

    Simplify answers

--- day1.py
+++ day1.py
@@ -3,62 +3,44 @@
 # By: Aiuraa using python
 #
 
-def read_input() -> str:
-    with open("input.txt") as f:
-        output = f.read()
-    
-    # append for not getting false positive
-    output += "\n"
-    return output
-
-def parse_input(a: str) -> list:
+def parse_input() -> list:
     sum_val = 0
     output = []
-    tmp_val = ""
 
-    for str_num in a:
-        if str_num == '\n' and tmp_val == '':
-            output.append(sum_val)
-            sum_val = 0
-            continue
-
-        if str_num == '\n':
-            sum_val += int(tmp_val)
-            # Uncomment to see how summing works
-            # print(f"Value: {int(tmp_val)} Sum: {sum_val}")
-            tmp_val = ""
-            continue
-
-        if str_num != '':
-            tmp_val += str_num
+    with open("input/day1.txt") as f:
+        for str_num in f.read().strip().split("\n"):
+            if str_num == '':
+                output.append(sum_val)
+                sum_val = 0
+                continue
+            
+            sum_val += int(str_num)
 
     return output
 
 # Part 1
-def find_answer_part_one(a: str) -> int:
-    input = parse_input(a)
+def find_answer_part_one(input: list) -> int:
     return max(input)
 
-def find_answer_part_two(a: str) -> int:
-    output = parse_input(a)
-    output.sort()
+# Part 2
+def find_answer_part_two(input: list) -> int:
+    input.sort()
 
-    print(f"Top 1: {output[-1]}")
-    print(f"Top 2: {output[-2]}")
-    print(f"Top 3: {output[-3]}")
+    print(f"Top 1: {input[-1]}")
+    print(f"Top 2: {input[-2]}")
+    print(f"Top 3: {input[-3]}")
 
-    return (output[-1] + output[-2] + output[-3])
-
-
+    return (input[-1] + input[-2] + input[-3])
 
 def main():
+    calories = parse_input()
+
     print("---- [Part 1] ----")
-    str_input = read_input()
-    output_1 = find_answer_part_one(str_input)
+    output_1 = find_answer_part_one(calories)
     print(f"Biggest value is: {output_1}")
 
     print("---- [Part 2] ----")
-    output_2 = find_answer_part_two(str_input)
+    output_2 = find_answer_part_two(calories)
     print(f"Combining results from top 3 is: {output_2}")
 
 if __name__ == '__main__':
